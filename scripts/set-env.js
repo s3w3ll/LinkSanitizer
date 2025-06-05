9e2f3f9ceb35ab6a5975d05177bb036e6d29b2c0
Hayden Sewell <hayden.sewell@gmail.com>
2025-06-05 02:35:48 +0000

    commit id

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -3,3 +3,4 @@ const fs = require("fs");
 const sha = process.env.VERCEL_GIT_COMMIT_SHA || "dev";
 fs.writeFileSync(".env.local", `NEXT_PUBLIC_COMMIT_SHA=${sha}\n`);
 console.log(`Wrote commit SHA to .env.local: ${sha}`);
+console.log("VERCEL_GIT_COMMIT_SHA from env:", process.env.VERCEL_GIT_COMMIT_SHA);
